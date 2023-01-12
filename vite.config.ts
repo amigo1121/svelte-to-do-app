f198d3af396569185a15dbf242ce48ea8ef921a9
Nam Nguyen <hoainam.hk14.cqt@gmail.com>
2023-01-12 22:11:09 +0000

    Add extension

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import type { UserConfig } from 'vite';
 const config: UserConfig = {
 	plugins: [sveltekit()],
 	server: {
-		port:5000,
-		hmr:{
-			clientPort: process.env.HMR_HOST ? 3344 : 5000,
-			host: process.env.HMR_HOST ? process.env.HMR_HOST?.substring("https://".length) : "localhost"
-		}
+		port:3000,
+		// hmr:{
+		// 	clientPort: process.env.HMR_HOST ? 3344 : 5000,
+		// 	host: process.env.HMR_HOST ? process.env.HMR_HOST?.substring("https://".length) : "localhost"
+		// }
 	}
 };
 
