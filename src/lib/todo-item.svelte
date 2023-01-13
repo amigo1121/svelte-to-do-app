9dfb81660522a94c656fba8c1ae7178d55e374d1
Nam Nguyen <hoainam.hk14.cqt@gmail.com>
2023-01-13 20:53:03 +0000

    create html structure

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,15 @@
+<div>
+	<form action="">
+		<input type="hidden" name="done" value="" />
+		<button aria-label="Mark done/ not done">Done/Not Done</button>
+	</form>
+
+	<form action="">
+		<input type="text" />
+		<button aria-label="save todo">Save</button>
+	</form>
+
+	<form action="">
+		<button aria-label="Delet todo">Delete</button>
+	</form>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import path from "path"
+    // @ts-ignore
+    import TodoItem from "$lib/todo-item.svelte";
+    const title = "Todos | New"
+</script>
+<svelte:head>
+    <title>
+        {title}
+    </title>
+</svelte:head>
+<h1>{title}</h1>
+<form action="" method="">
+    <input type="text" name="text" placeholder="+ tpye to add todo" aria-label="add-todo">
+</form>
+<TodoItem></TodoItem>
+<TodoItem></TodoItem>
+<TodoItem></TodoItem>
+<TodoItem></TodoItem>
